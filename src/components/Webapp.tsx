3aca9ab13e6e1b3366a3b62df9bf6cbc3aff2642
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2025-04-25 01:00:16 -0400

    add guia rapida
    para pre lanzamiento, publicacion de guia descargable gratuita de posicionamiento

--- src/components/Webapp.tsx
+++ src/components/Webapp.tsx
@@ -59,16 +59,6 @@ export default function Webapp() {
 
         },[]);
 
-  const proyecto = {
-    nombre: "Kick Starter Media Kit",  
-    descripcion: "¿Sueñas con lanzar tu app y hacerla crecer? El Kick Starter Media Kit es tu solución integral. ✨  Obtén un potente generador de apps intuitivo y una colección curada de recursos de marketing esenciales, todo en un solo lugar. Ideal para emprendedores que buscan una manera fácil y efectiva de dar sus primeros pasos en el mundo digital. ¡Tu viaje emprendedor nunca ha sido tan accesible!" ,
-    mision: "Nuestra Misión: Empoderar a la próxima generación de emprendedores, eliminando las barreras económicas para convertir sus sueños en realidad. 💡  Creemos en un mundo donde la innovación no se vea limitada por el presupuesto. Estamos comprometidos a ofrecer soluciones accesibles que permitan a cada emprendedor alcanzar su máximo potencial. Estamos aquí para democratizar el emprendimiento, ofreciendo las herramientas necesarias al costo más accesible. 🛠️  Nuestra misión es allanar el camino para que las ideas brillantes florezcan, sin que el factor económico sea un obstáculo. Queremos ver cómo tu pasión transforma el mundo.",
-    vision: "Nuestra Visión: Derribar la barrera tecnológica en el emprendimiento a través de la automatización inteligente, creando un entorno donde el costo ya no sea un impedimento para la innovación y el crecimiento. 💥  Estamos construyendo un futuro donde la tecnología empodera a cada emprendedor, permitiendo que sus ideas transformen el mundo sin las limitaciones económicas del pasado.",
-    logo: "logo.png"    
-   
-  }
-
-
     if(isLoadingP){
     return <p>Cargando WebApp Data...</p>;
     }
@@ -96,6 +86,9 @@ export default function Webapp() {
             <p className="welcome-subtitle">{fraseProyecto}</p>
             <div className="welcome-form">
                 <a href="https://wa.me/56920905973?text=Quiero%20preRagistrarme%20al%20lanzamiento%20del%20eWave%20Pack"><button className="welcome-button">Registro Anticipado aqui!</button></a>
+            </div>
+            <div className="welcome-form">
+                <a href="Guia-rapida-Presencia-online-Santiago-2025-EpicMediaWave.pdf"><button className="descarga-guia">Descarga la guía gratis!</button></a>
             </div>
         </div>
     </section>
